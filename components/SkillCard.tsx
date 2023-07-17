5cef7ebd26de1dfacd85c70452b7bc10dfb49306
Thân Văn Hiếu <vanhieuu99@gmail.com>
2023-07-17 23:58:41 +0700

    feat: skill card

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const SkillCard = (props: Props) => {
   return (
-    <div className='lg:flex gap-10 bg-ct-primary-02'>
+    <div className='lg:flex gap-10 bg-ct-primary-02 rounded'>
       <h3 className='text-lg font-medium pt-8 pb-2  '>{props.name}</h3>
       <p className="text-ct-primary-text py-1">Photoshop</p>
     </div>
